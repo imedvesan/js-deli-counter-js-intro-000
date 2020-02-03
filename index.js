f5b2e4713dc9fe1252d5f3d5135a1a9dfa0fd899
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-02-03 16:21:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function takeANumber(katzDeli,personName) {
-  otherDeli.push(katzDeli);
-  return `Welcome, ${personName}. You are number ${katzDeli} in line.`;
+
+var katzDeliLine =[];
+function takeANumber(katzDeliLine,name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.lenght} in line.`;
 }
