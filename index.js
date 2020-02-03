ceaf81e80d68f97c2844797f4cadf9fb6015573e
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-02-03 17:13:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function takeANumber(katzDeliLine,name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.lenght} in line.`;
 }
+
+function nowServing (katzDeliLine) {
+  katzDeliLine.pop(name);
+  return katzDeliLine;
+} if (katzDeliLine.lenght === 0) {
+  return "There is nobody waiting to be served!"
+}
